0d899b38af7110904f6572ffa1508809458c6b91
shubham Kumar <sktovisit@gmail.com>
2022-03-15 12:30:10 +0530

    visitors_count_added

--- javascript/script.js
+++ javascript/script.js
@@ -644,16 +644,8 @@ projectManagerBtn.addEventListener("click", () => {
 });
 
 // for counting visitors
-const countEl = document.getElementById('count');
-
-updateVisitCount();
-
-function updateVisitCount() {
-	fetch('https://api.countapi.xyz/update/mroptimizer/theoptimizer/?amount=1')
-	.then(res => res.json())
-	.then(res => {
-		countEl.innerHTML = res.value;
-	})
+function websiteVisits(response) {
+  document.querySelector("#visits").textContent = response.value;
 }
 
 new Ribbons({
